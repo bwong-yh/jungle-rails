c24235230ece9f2c35c0f4e759a65a50ef1b3fbd
Billy Wong <bwong.yh@protonmail.com>
2022-08-24 15:34:18 -0600

    able to add admin categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  
   def show
-    @categories = Category.all
+    @categories = Category.order(id: :asc).all
     @products = Product.all
   end
 end
