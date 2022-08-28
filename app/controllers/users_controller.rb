6666dddb205562b6b6753107f588654babb38d73
Billy Wong <bwong.yh@protonmail.com>
2022-08-28 17:36:51 +0900

    handle error for duplicate email at registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-   
-    if @user.save
+
+    @duplicate_email = User.all.filter do |u|
+      u.email == @user.email
+    end
+
+    if @duplicate_email.length > 0
+      flash[:alert] = "Email already registered."
+      render :new
+    elsif @user.save
       session[:user_id] = @user.id
-      redirect_to '/', notice: "Account created"
-    else
+      redirect_to '/'
+    else 
       render :new
     end
   end
