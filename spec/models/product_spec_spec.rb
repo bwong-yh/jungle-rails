aff29366a2c2dd83393cb59dcdaf94bf36564fc0
Billy Wong <bwong.yh@protonmail.com>
2022-09-13 06:32:57 +0900

    test product name missing

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe Product, type: :model do
     it "should create a product" do
       @category = Category.new(name: "testCategory")
       @product = Product.new(name: "testProduct", price: 99, quantity: 10, category: @category)
-      expect(@product.errors).not_to include("can't be blank")
+      expect(@product.valid?).to be(true)
+    end
+
+    it "should not create a product if product name is missing" do
+      @category = Category.new(name: "testCategory")
+      @product = Product.new(name: nil, price: 99, quantity: 10, category: @category)
+      @product.valid? # necessary ensure valid is false so value can be assigned to the error
+      expect(@product.errors[:name]).to include("can't be blank")
     end
   end
 end
