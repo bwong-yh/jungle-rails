0fd0e272138a8911e854f33f434df82ff7911379
Billy Wong <bwong.yh@protonmail.com>
2022-09-14 03:15:51 +0900

    test email and names validation

--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -7,16 +7,22 @@ RSpec.describe User, type: :model do
       expect(@user.valid?).to be(true)
     end
 
-    it "should not create a user password is missing" do
-      @user = User.new(first_name: "John", last_name: "Smith", email: "johnsmith@email.com", password_confirmation: "test1234")
+    it "should not create a user if first name is missing" do
+      @user = User.new(last_name: "Smith", email: "johnsmith@email.com",password_confirmation: "test1234")
       @user.valid?
-      expect(@user.errors[:password]).to include("can't be blank")
+      expect(@user.errors[:first_name]).to include("can't be blank")
     end
 
-    it "should not create a user password confirmation does not match" do
-      @user = User.new(first_name: "John", last_name: "Smith", email: "johnsmith@email.com", password: "test1234", password_confirmation: "test4321")
+    it "should not create a user if last name is missing" do
+      @user = User.new(first_name: "John", email: "johnsmith@email.com",password_confirmation: "test1234")
       @user.valid?
-      expect(@user.errors[:password_confirmation]).to include("doesn\'t match Password")
+      expect(@user.errors[:last_name]).to include("can't be blank")
+    end
+
+    it "should not create a user if email is missing" do
+      @user = User.new(first_name: "John", last_name: "Smith", password_confirmation: "test1234")
+      @user.valid?
+      expect(@user.errors[:email]).to include("can't be blank")
     end
 
     it "should not create a user if email is not unique (email already taken)" do
@@ -25,6 +31,18 @@ RSpec.describe User, type: :model do
       @user2 = User.new(first_name: "John", last_name: "Smith", email: "JohnSmith@email.com", password: "test4321", password_confirmation: "test4321")
       @user2.valid?
       expect(@user2.errors[:email]).to include("has already been taken")
+    end
+
+    it "should not create a user if password is missing" do
+      @user = User.new(first_name: "John", last_name: "Smith", email: "johnsmith@email.com", password_confirmation: "test1234")
+      @user.valid?
+      expect(@user.errors[:password]).to include("can't be blank")
+    end
+
+    it "should not create a user if password confirmation does not match" do
+      @user = User.new(first_name: "John", last_name: "Smith", email: "johnsmith@email.com", password: "test1234", password_confirmation: "test4321")
+      @user.valid?
+      expect(@user.errors[:password_confirmation]).to include("doesn\'t match Password")
     end
   end
 end
